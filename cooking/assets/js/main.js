53ef46147d1b3246bc4d130babfcfbecaf7bfa35
BrellaH <33139496+BrellaH@users.noreply.github.com>
2021-09-17 17:42:09 -0400

    simple ui for user, where form can be submited by enter, and delete entry by clicking on the item

--- cooking/assets/js/main.js
+++ cooking/assets/js/main.js
@@ -1,29 +1,27 @@
-const ingredients = []
-ingredients_aside = document.getElementById("current_ingredient_display");
+const ing_list = [];
+const ing_ul = document.querySelector("#current_ingredient_display")
+const ing_submit = document.querySelector("#ing_submit")
 
-function loadCurrentIngredientList() {
-    if (ingredients_aside.childNodes.length != 0) {
-        ingredients_aside.removeChild(ingredients_aside.childNodes[0])
-    }
-    ingredients_ul = document.createElement("ul")
-    ingredients_aside.appendChild(ingredients_ul)
-    for (i = 0; i < ingredients.length; i++) {
-        listItem = document.createElement("li");
-        listItem.innerHTML = ingredients[i]
-        ingredients_ul.appendChild(listItem)
-    }
+const delFromList = function (i) {
+    ing_list.splice(ing_list.indexOf(i), 1)
 }
 
-function addIngredient() {
-    ing = document.getElementById("ing").value
-    ingredients.push(ing)
-    loadCurrentIngredientList()
-}
-
-function deleteIngredient() {
-    ing = document.getElementById("ing").value
-    if (ingredients.indexOf(ing) != -1) {
-        ingredients.splice(ingredients.indexOf(ing), 1)
+ing_submit.addEventListener("submit", function (e) {
+    e.preventDefault()
+    if (ing_list.includes(ing_submit.ing_input.value) === false && ing_submit.ing_input.value != "") {
+        ing_list.push(ing_submit.ing_input.value)
+        let li = document.createElement("li")
+        li.textContent = ing_submit.ing_input.value
+        li.id = ing_submit.ing_input.value
+        ing_ul.appendChild(li)
     }
-    loadCurrentIngredientList()
-}
+    ing_submit.ing_input.value = ""
+})
+ing_ul.addEventListener("click", function (e) {
+    if (e.target.nodeName === "LI") {
+        if (confirm(`Out of water ${e.target.innerText}?`)) {
+            e.target.remove()
+            delFromList(e.target.innerText)
+        }
+    }
+})
